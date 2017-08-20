6b5ea39686029d0a7fc4adf0246f67861511b641
Evgeny Goncharov <NikeLambert@gmail.com>
2017-08-20 16:08:00 +0300

    Проетк для автоматичского управления вентилятором на rpi

---
+++ fan.py
@@ -0,0 +1,40 @@
+import signal
+from time import sleep
+from datetime import datetime
+from threading import Event
+
+import RPi.GPIO as GPIO
+
+
+__author__ = "Evgeny Goncharov"
+
+run_service = Event()
+run_service.set()
+
+
+def handler(signum, frame):
+    run_service.clear()
+
+
+signal.signal(signal.SIGTERM, handler)
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+RELAY = 20
+
+GPIO.setup(RELAY, GPIO.OUT, initial=GPIO.LOW)
+
+while run_service.is_set():
+    if 6 <= datetime.now().hour < 22:
+        # relay - on, fan - on
+        GPIO.output(RELAY, GPIO.HIGH)
+
+    else:
+        # relay - off, fan - off
+        GPIO.output(RELAY, GPIO.LOW)
+
+    sleep(60)
+
+GPIO.cleanup(RELAY)
